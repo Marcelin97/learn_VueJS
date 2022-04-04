e1bc067932947737d7227002b6b4c1bef085ed1c
Marcelin97 <lois_m@outlook.com>
2022-04-04 15:56:08 +0200

    review nav in the app.js and review AboutView.js

--- my-first-vue-cli-app/src/App.vue
+++ my-first-vue-cli-app/src/App.vue
@@ -1,9 +1,11 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/items">Items</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <div id="app">
+    <nav id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/items">Items</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+  </div>
   <router-view />
 </template>
 
@@ -18,5 +20,17 @@
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
--- my-first-vue-cli-app/src/views/AboutView.vue
+++ my-first-vue-cli-app/src/views/AboutView.vue
@@ -1,5 +1,36 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <section>
+    <h1>A propos de nous</h1>
+    <div>
+      <h2>Contactez nous</h2>
+      <p>Adresse : {{ address }}</p>
+      <p>Téléphone : {{ phone }}</p>
+      <p>Email : {{ email }}</p>
+      <p>Horaires :</p>
+      <ul>
+        <li>L-V: 06:00 à 16:00</li>
+        <li>Samedi: 07:00 à 14:00</li>
+        <li>Dimanche: 07:00 à 12:00</li>
+      </ul>
+    </div>
+  </section>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      address: "18 avenue du Beurre, Paris, France",
+      email: "hello@cafewithavue.bakery",
+      phone: "01 88 88 88 88",
+    };
+  },
+};
+</script>
+
+<style scoped>
+h2{
+  font-size:1.2em;
+  color:rgba(0, 8, 255, 0.744);
+}
+</style>
