8c19a734f6270782377232ed3ea12f844f515548
Marcelin97 <lois_m@outlook.com>
2022-04-04 15:01:00 +0200

    start to create a new projet with vue-cli

---
+++ my-first-vue-cli-app/src/views/ItemsView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="product">
+    <ItemsWorld/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import ItemsWorld from '@/components/ItemsWorld.vue'
+
+export default {
+  name: 'ItemsView',
+  components: {
+    ItemsWorld
+  }
+}
+</script>
